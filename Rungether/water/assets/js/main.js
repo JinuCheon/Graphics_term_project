7df15a48a385cdd23a10d6a2bf975fb2361a6780
JinuCheon(깻묵) <woojin8787@gmail.com>
2022-10-28 01:13:43 +0900

    feat: add water shader demo

---
+++ Rungether/water/assets/js/main.js
@@ -0,0 +1,30 @@
+function mainLoop() {
+	requestAnimationFrame(mainLoop);
+	DEMO.update();
+}
+
+$(function() {
+	WINDOW.initialize();
+
+	var parameters = {
+		alea: RAND_MT,
+		generator: PN_GENERATOR,
+		width: 2000,
+		height: 2000,
+		widthSegments: 250,
+		heightSegments: 250,
+		depth: 1500,
+		param: 4,
+		filterparam: 1,
+		filter: [ CIRCLE_FILTER ],
+		postgen: [ MOUNTAINS_COLORS ],
+		effect: [ DESTRUCTURE_EFFECT ]
+	};
+	
+	DEMO.initialize('canvas-3d', parameters);
+	
+	WINDOW.resizeCallback = function(inWidth, inHeight) { DEMO.resize(inWidth, inHeight); };
+	DEMO.resize(WINDOW.ms_Width, WINDOW.ms_Height);
+	
+	mainLoop();
+});
